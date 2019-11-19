e1fc0b1242aa706893c6f9c03bcc5a781b073026
Raphi1524694 <raphaellubaschewski@gmail.com>
2019-11-19 11:58:44 +0100

    batch script for ex03

---
+++ PCA_03/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=ex03
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=02:00:00
+#SBATCH --output=out.txt
+
+#module purge > /dev/null 2>&1
+
+module load cmake
+module load gcc
+
+# comment in if you also want to compile
+# cmake .
+# make
+
+./PCA_03 100 35 127.0
