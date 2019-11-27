49bb5c8ae9164fe1301ce74629ddb8b3c34f0815
Raphi1524694 <raphaellubaschewski@gmail.com>
2019-11-27 21:05:59 +0100

    added exercise 01

---
+++ PCA_01/PCA/PCA.cpp
@@ -0,0 +1,30 @@
+﻿#include "PCA.h"
+using namespace std;
+
+
+double approximatePi(int n) {
+	double deltaX = 1.0 / n;
+	double result = 0.0;
+	double x, y;
+	// only measure time for actual approximation
+	auto start = chrono::high_resolution_clock::now();
+	for (int i = 0; i < n; i++) {
+		x = i * deltaX;
+		y = sqrt(1.0 - pow(x, 2));
+		result += y * deltaX;
+	}
+	result *= 4.0;
+	auto end = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+	cout << "The approximation of Pi with " << n << " rectangles" << " took " << duration << " milliseconds" << endl;
+	return result;
+}
+
+
+int main(int argc, char* argv[]) { 
+	int n = 1000000;
+	double result = approximatePi(n);
+	double pi = 3.1415926535897;
+	cout << "The result is " << result << endl;
+	cout << "The exact value of pi (13 decimals) is " << pi << endl;
+}
---
+++ PCA_01/PCA/PCA.h
@@ -0,0 +1,10 @@
+﻿// PCA.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <chrono>
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
