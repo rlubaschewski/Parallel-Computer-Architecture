468ee33643fc9db941fffb07ae177dccbe3dfbb8
Raphi1524694 <raphaellubaschewski@gmail.com>
2019-11-27 02:20:37 +0100

    started exercise 02

---
+++ PCA_02/PCA_02.cpp
@@ -0,0 +1,85 @@
+﻿#include "PCA_02.h"
+
+using namespace std;
+
+typedef vector<vector<double> > matrix;
+typedef vector<double> vec;
+typedef chrono::time_point<chrono::steady_clock> timePoint;
+
+double getRandomValue()
+{
+	return (double) rand() / RAND_MAX;
+}
+
+void matrixVectorMultiplyRowWise(matrix &A, vec &x, vec &y) {
+	for (int i = 0; i < A.size(); i++) {
+		for (int j = 0; j < A[0].size(); j++) {
+			y[i] += A[i][j] * x[j];
+		}
+	}
+}
+
+void matrixVectorMultiplyColumnWise(matrix& A, vec& x, vec& y) {
+	for (int j = 0; j < A[0].size(); j++) {
+		for (int i = 0; i < A.size(); i++) {
+			y[i] += A[i][j] * x[j];
+		}
+	}
+}
+
+void printVector(vec& y) {
+	for (int i = 0; i < y.size(); i++) {
+		printf("%f\n", y[i]);
+	}
+}
+
+void printMatrix(matrix& A) {
+	for (int i = 0; i < A.size(); i++) {
+		for (int j = 0; j < A[0].size(); j++) {
+			printf("%f ", A[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	int m = atoi(argv[1]);
+	int n = atoi(argv[2]);
+	bool columnWise = argv[3] == "columnWise";
+	srand(time(NULL));
+	matrix A(m, vec(m, 0.0));
+
+	// fill matrix with random values
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			A[i][j] = getRandomValue();
+		}
+	}
+	vec x(m, 0.0);
+
+	// fill vector with random values
+	for (int i = 0; i < m; i++) {
+		x[i] = getRandomValue();
+	}
+
+	// result vector
+	vec y(m, 0.0);
+	timePoint start;
+	timePoint end;
+	if (columnWise) {
+		start = chrono::high_resolution_clock::now();
+		matrixVectorMultiplyColumnWise(A, x, y);
+		end = chrono::high_resolution_clock::now();
+	}
+	else {
+		start = chrono::high_resolution_clock::now();
+		matrixVectorMultiplyRowWise(A, x, y);
+		end = chrono::high_resolution_clock::now();
+	}
+	auto duration = chrono::duration_cast<chrono::microseconds >(end - start);
+	cout << "The execution time of the multiplication with a" << m << " x " n << "matrix took " << duration "micro seconds."
+	
+
+	return 0;
+}
---
+++ PCA_02/PCA_02.h
@@ -0,0 +1,12 @@
+﻿// PCA_02.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h> 
+#include<chrono>
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
