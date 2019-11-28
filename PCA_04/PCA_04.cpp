d364a7af095f2b8631e552dbefac1ca62da5dd02
rlubaschewski <raphaellubaschewski@gmail.com>
2019-11-28 23:31:32 +0100

    added mutex for critical section

--- PCA_04/PCA_04.cpp
+++ PCA_04/PCA_04.cpp
@@ -10,51 +10,58 @@ struct runnerStruct {
 	int n;
 	int start;
 	int end;
-	double result;
 };
+
+double result = 0.0;
+pthread_mutex_t locker = PTHREAD_MUTEX_INITIALIZER;
 
 // thread function to generate partial sums of pi approximation
 void* approximatePiRunner(void* arg) {
 	struct runnerStruct *arg_struct = (struct runnerStruct*)arg;
 	double deltaX = 1.0 / arg_struct -> n;
-	double x, y, result = 0.0;
+	double x, y, partialResult = 0.0;
 	for (int i = arg_struct -> start; i <= arg_struct -> end; i++) {
 		x = i * deltaX;
 		y = sqrt(1.0 - pow(x, 2));
-		result += y * deltaX;
+		partialResult += y * deltaX;
 	}
-	arg_struct -> result = result;
+	// start critical section
+	pthread_mutex_lock(&locker);
+	result += partialResult;
+	// end critical section
+	pthread_mutex_unlock(&locker);
 	pthread_exit(0);
 }
 
 
 int main(int argc, char* argv[]) {
-	int n = 1000;
-	int numberOfThreads = 200;
+	int n = 1000000;
+	int numberOfThreads = 32;
 
 	pthread_t threadIds[numberOfThreads];
 
 	struct runnerStruct runnerData[numberOfThreads];
-
+	auto start = chrono::high_resolution_clock::now();
+	// spawn all threads
 	for (int i = 0; i < numberOfThreads; i++) {
 		runnerData[i].start = i * (n / numberOfThreads);
-		cout << "start of thread " << i + 1 << " is " << runnerData[i].start << endl;
 		runnerData[i].end  = (i + 1) * (n / numberOfThreads) - 1;
-		cout << "end of thread " << i + 1 << " is " << runnerData[i].end << endl;
 		runnerData[i].n = n;
 
 		pthread_attr_t threadAtrribute;
 		pthread_attr_init(&threadAtrribute);
 		pthread_create(&threadIds[i], &threadAtrribute, approximatePiRunner, &runnerData[i]);
 	}
-	double result = 0.0;
+	// wait for threads to complete
 	for (int i = 0; i < numberOfThreads; i++) {
 		pthread_join(threadIds[i], NULL);
-		result += runnerData[i].result;
 	}
 	result *= 4.0;
+	auto end = chrono::high_resolution_clock::now();
 	double pi = 3.1415926535897;
 	cout.precision(17);
 	cout << "The result is " << result << endl;
 	cout << "The exact value of pi is " << pi << endl;
+	auto duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+	cout << "The approximation of Pi with " << n << " rectangles and " << numberOfThreads << " threads " << " took " << duration << " microseconds" << endl;
 }
